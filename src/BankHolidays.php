eaea6cb57091fecac0396805159c0a6afcfc3530
mika <1506612+elmika@users.noreply.github.com>
2019-10-08 23:44:40 +0200

    Implemented BankHolidays basic methods.

--- src/BankHolidays.php
+++ src/BankHolidays.php
@@ -17,11 +17,42 @@ class BankHolidays
 	* @var $bankHolidays \DateTime[] or string[]
 	*/
 	public function __construct(DateRange $range, array $bankHolidays)
-	{				
+	{		
+		$this->dayRange = $range;
+		foreach($bankHolidays as $bankHoliday)
+		{
+			$this->add($bankHoliday);
+		}
+	}
+
+	private function add($bankHoliday)
+	{
+		if($this->isBankHoliday($bankHoliday))
+		{
+			return;
+		}
+
+		$bankHoliday = $this->normalizeDay($bankHoliday);
+		$this->bankHolidays[]=$bankHoliday;
+	}
+
+	private function normalizeDay($date) : \DateTime
+	{
+		if( ! $date instanceof \DateTime)
+		{
+			$date = new \DateTime($date);
+		}
+		return $date->setTime(0,0,0,0);
 	}
 
 	public function isBankHoliday($date) : bool
 	{
-		return false;
+		if( ! $this->dayRange->isWithinRange($date))
+		{
+			throw new InvalidDateRangeException("Bank holiday is out of date range", 1);
+		}
+		$date = $this->normalizeDay($date);
+
+		return in_array($date, $this->bankHolidays);
 	}
 }
