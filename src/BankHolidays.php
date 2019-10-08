172673bd8a4a9addd25ef90de6ff88decab2ff5c
mika <1506612+elmika@users.noreply.github.com>
2019-10-08 09:01:41 +0200

    Built test for BankHolidays object

--- src/BankHolidays.php
+++ src/BankHolidays.php
@@ -4,6 +4,24 @@ declare(strict_types=1);
 
 namespace SquaredPoint\BankHolidays;
 
+use SquaredPoint\BankHolidays\Value\DateRange;
+use SquaredPoint\BankHolidays\Exception\InvalidDateRangeException;
+
 class BankHolidays
 {
+	private $bankHolidays = [];
+	private $dayRange;
+
+	/**
+	* @var $range DateRange
+	* @var $bankHolidays \DateTime[] or string[]
+	*/
+	public function __construct(DateRange $range, array $bankHolidays)
+	{				
+	}
+
+	public function isBankHoliday($date) : bool
+	{
+		return false;
+	}
 }
