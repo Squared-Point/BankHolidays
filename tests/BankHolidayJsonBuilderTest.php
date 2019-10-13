1c7a8629e5188b4d7d57117fb846e30aac8ae685
mika <1506612+elmika@users.noreply.github.com>
2019-10-14 00:06:30 +0200

    Set up tests for bank holiday builder minimalist json format.

--- tests/BankHolidayJsonBuilderTest.php
+++ tests/BankHolidayJsonBuilderTest.php
@@ -9,6 +9,10 @@ use SquaredPoint\BankHolidays\BankHolidayJsonBuilder;
 
 class BankHolidayJsonBuilderTest extends TestCase
 {
+
+    const FORMAT_JSON = false;
+    const FORMAT_ARRAY = true;
+
     protected function setUp() : void
     {
  
@@ -34,6 +38,75 @@ class BankHolidayJsonBuilderTest extends TestCase
         ];
     }
 
+    private function getSimpleBankHolidaySetup($format=self::FORMAT_JSON)
+    {
+        $json =  '{
+            "name": "Spain",
+            "bank_holidays":
+            [
+                "2019-01-01"
+            ]            
+        }';
+
+        return ($format == self::FORMAT_JSON) ? $json : json_decode($json, true);
+        
+    }
+
+    public function correctFormatProvider()
+    {        
+        $a = $this->getSimpleBankHolidaySetup();
+        
+        $b = $this->getSimpleBankHolidaySetup(self::FORMAT_ARRAY);
+        unset($b["name"]);
+        $b = json_encode($b);
+
+        $c = $this->getSimpleBankHolidaySetup(self::FORMAT_ARRAY);
+        $c["bank_holidays"][] = "2019-01-05";
+        $c = json_encode($c);
+
+        $d = $this->getSimpleBankHolidaySetup(self::FORMAT_ARRAY);
+        $d["bank_holidays"][] = "2019-01-01";
+        $d = json_encode($d);
+        
+
+        return [            
+            [$a],
+            [$b], 
+            [$c],
+        ];
+    }
+
+    public function incorrectFormatProvider()
+    {
+
+        $a = $this->getSimpleBankHolidaySetup(self::FORMAT_ARRAY);
+        unset($a["bank_holidays"]);
+        $a = json_encode($a);
+
+        $b = $this->getSimpleBankHolidaySetup(self::FORMAT_ARRAY);
+        $b["dummy"] = "unknown field";
+        $b = json_encode($b);
+
+        $c = $this->getSimpleBankHolidaySetup(self::FORMAT_ARRAY);
+        $c["bank_holidays"][] = "wrong date format";
+        $c = json_encode($c);
+
+        $d = $this->getSimpleBankHolidaySetup(self::FORMAT_ARRAY);
+        $d["bank_holidays"] = "not an array";
+        $d = json_encode($d);
+
+        $e = $this->getSimpleBankHolidaySetup(self::FORMAT_ARRAY);
+        $e["name"] = ["not a string", "at all"];
+        $e = json_encode($d);
+
+        return [
+            [$a],
+            [$b],
+            [$c],
+            [$d]
+        ];
+    }
+
     /**
     * @dataProvider correctJsonProvider
     */
@@ -48,6 +121,25 @@ class BankHolidayJsonBuilderTest extends TestCase
     * @expectedException SquaredPoint\BankHolidays\Exception\JsonDecodeException
     */
     public function testIncorrectJsonParsing($s) : void
+    {
+        $builder = new BankHolidayJsonBuilder($s);
+        $this->assertTrue(false);
+    }
+
+    /**
+    * @dataProvider correctFormatProvider
+    */
+    public function testCorrectFormat($s) : void
+    {
+        $builder = new BankHolidayJsonBuilder($s);
+        $this->assertTrue(true);
+    }
+
+    /**
+    * @dataProvider incorrectFormatProvider
+    * @expectedException SquaredPoint\BankHolidays\Exception\BankHolidayFormatException
+    */
+    public function testIncorrectFormat($s) : void
     {
         $builder = new BankHolidayJsonBuilder($s);
         $this->assertTrue(false);
